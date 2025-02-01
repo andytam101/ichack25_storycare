247ffbcd1870c6ead379e61673c3232a97119b5c
Andy Tam <andytam321@gmail.com>
2025-02-01 18:38:39 +0000

    text generation stuff

--- backend/app.py
+++ backend/app.py
@@ -14,12 +14,18 @@ def prompt():
     if request.method == "POST":
         # takes in prompt from frontend
 
-        # pass into LLM to get complex story
+        # pass into LLM to get complex story split into n short chapters
 
-        # pass into image generator to get stuff
+        # convert chapters into text-to-image prompt
+
+        # pass into image generator to get images
 
         # pass into LLM again to make story simpler
 
         # output result
 
         pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
