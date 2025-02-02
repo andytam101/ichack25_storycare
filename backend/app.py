1d607b2d2e2047d444e9738acc28ddfd47401a40
Andy Tam <andytam321@gmail.com>
2025-02-02 03:53:19 +0000

    remake backend

--- backend/app.py
+++ backend/app.py
@@ -1,42 +1,28 @@
 from flask import Flask, request, jsonify
-from text_generation import generate_story_text, generate_chapter_to_prompt, shorten_text
-from img_generation import generate_image
-import json
-
+from generate_content import generate_image, generate_short_story, generate_dalle_prompt
 
 app = Flask(__name__)
 
+
 @app.route("/prompt", methods=["POST"])
 def prompt():
-    if request.method == "POST":
-        prompt = request.form["prompt"] # change this to whatever matches the frontend
-        
-        with open("sample.json", "r") as f:
-            data = json.load(f)
-            f.close()
+    user_prompt = request.form["prompt"]
+    # might add type in later (i.e. test, disease or treatment)
+    paragraphs, images = generate_output(user_prompt)
 
-        # list of dictionaries containing (revised_prompt, url)
-        _, _, all_images_data, all_captions = get_images_and_captions_from_user_prompt(prompt, data)   
-        return jsonify({
-            "all_images": all_images_data,
-            "all_captions": all_captions
-        })
+    return jsonify({
+        "captions": paragraphs,
+        "images": images
+    })
 
 
-def get_images_and_captions_from_user_prompt(prompt, commands):
-    print("Generating story")
-    paragraphs = generate_story_text(prompt, commands)
+def generate_output(prompt):
+    paragraphs = generate_short_story(prompt)
     paragraphs = paragraphs.split("\n\n")
-    print("Generating prompts")
-    img_prompts = list(map(lambda x: generate_chapter_to_prompt(x, commands), paragraphs))
-    print("Generating images")
-    images_data = list(map(lambda x: generate_image(x, commands), img_prompts))
-    print("Generating summaries")
-    summarised_text = list(map(lambda x: shorten_text(x, commands), paragraphs))
-    return paragraphs, img_prompts, images_data, summarised_text
-
+    dalle_prompts = list(map(lambda x: generate_dalle_prompt(x), paragraphs))
+    images = list(map(lambda x: generate_image(x), dalle_prompts))
+    
+    return paragraphs, images
 
 if __name__ == "__main__":
-    # app.run(debug=True)
-    paras, prompts, images, summaries = get_images_and_captions_from_user_prompt("My child has asthma. Show me the diagnosis")
-
+    app.run(debug=True)
